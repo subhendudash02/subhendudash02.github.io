774d4d4c88bc077b1b99fb9692c0916ac79dba01
Subhendu Dash <subhendu.dash2020@vitstudent.ac.in>
2022-06-19 22:52:38 +0530

    add about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,6 +6,25 @@ export default function About() {
     return (
         <div className="about">
             <h1>About</h1>
+            <div className="body">
+                <p>Hey there! I am Subhendu Dash, currently an undergrad student in VIT Chennai, persuing Electrical Engineering.</p>
+                <p>I usually create interesting stuffs in my leisure time, and that's how my coding journey started.
+                    I started with web development when I was in high school and then I started programming in Python.
+                </p>
+                <p>
+                    I never left my coding journey because I enjoyed learning new stuffs on the go.
+                </p>
+                <p>
+                    I love to watch movies and a lots of web series which feels refreshing to me.
+                </p>
+                <p>
+                    I play sports occasionally, such as football and badminton. Mostly I play computer games :P.
+                </p>
+                <p>
+                    That's all about me ;). My motto is to be happy always.
+                </p>
+                Peace ✌️
+            </div>
         </div>
     );
 }
