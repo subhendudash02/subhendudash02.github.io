76ee491b4fcdc622e79c045726b4d631d45839ef
Subhendu Dash <subhendu.dash2020@vitstudent.ac.in>
2022-10-26 21:50:20 +0530

    add timeline, rough figure, not for mobile

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,8 +1,27 @@
-
+import "../styles/work.css";
 
 export default function Work() {
     return (
-        <div className="work">
+        <div className="work line">
+            <div className="card center">
+                <h1>Hello</h1>
+            </div>
+
+            <div className="card left">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam placeat qui velit nisi temporibus officiis, laborum nostrum? In, voluptates commodi? Sapiente beatae maiores quidem debitis magni maxime aliquid accusantium dolores?</p>
+            </div>
+
+            <div className="card right">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus esse eaque quaerat libero? Distinctio recusandae, asperiores labore ratione alias id dolorem quasi maxime, ut voluptatem perferendis eos quia quod illo.</p>
+            </div>
+
+            <div className="card left">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam placeat qui velit nisi temporibus officiis, laborum nostrum? In, voluptates commodi? Sapiente beatae maiores quidem debitis magni maxime aliquid accusantium dolores?</p>
+            </div>
+
+            <div className="card right">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus esse eaque quaerat libero? Distinctio recusandae, asperiores labore ratione alias id dolorem quasi maxime, ut voluptatem perferendis eos quia quod illo.</p>
+            </div>
             
         </div>
     );
