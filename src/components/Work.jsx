62c7769d9a4cce83542454e729d4de22495317c0
Subhendu Dash <subhendu.dash2020@vitstudent.ac.in>
2022-11-27 09:28:24 +0530

    modify timeline a bit

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -13,34 +13,35 @@ export default function Work() {
                 <li className="flowBox">
                     <h2>{description.college.name}</h2>
                     <i>{description.college.year}</i>
-                    <p>{description.college.desc[0]}</p>
-                    <p>{description.college.desc[1]}</p>
-                    <p>{description.college.desc[2]}</p>
-                    <p>{description.college.desc[3]}</p>
+                    
+                    { description.college.desc.map(i => {
+                        return <p>{i}</p>;
+                    }) }
                 </li>
 
                 <li className="flowBox">
                     <h2>{description.school.name}</h2>
                     <i>{description.school.year}</i>
-                    <p>{description.school.desc[0]}</p>
+                    { description.school.desc.map(i => {
+                        return <p>{i}</p>;
+                    }) }
                 </li>
-            </ul>
 
-            <ul className="flowDesc">
                 <VolunteerActivismIcon className="flowIcon" fontSize="large"></VolunteerActivismIcon>
                 <li className="flowBox">
                     <h2>{description.hc.name}</h2>
                     <i>{description.hc.year}</i>
-                    <p>{description.hc.desc[0]}</p>
-                    <p>{description.hc.desc[1]}</p>
+                    { description.hc.desc.map(i => {
+                        return <p>{i}</p>;
+                    }) }
                 </li>
 
                 <li className="flowBox">
                     <h2>{description.gwoc.name}</h2>
                     <i>{description.gwoc.year}</i>
-                    <p>{description.gwoc.desc[0]}</p>
-                    <p>{description.gwoc.desc[1]}</p>
-                    <p>{description.gwoc.desc[2]}</p>
+                    { description.gwoc.desc.map(i => {
+                        return <p>{i}</p>;
+                    }) }
                 </li>
             </ul>
         </div>
