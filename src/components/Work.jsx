bc50b4baf3b1663f72f44bb53da5b233179a6097
Subhendu Dash <subhendu.dash2020@vitstudent.ac.in>
2022-11-29 10:34:51 +0530

    unique key prop setting in map

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -13,17 +13,17 @@ export default function Work() {
                 <li className="flowBox">
                     <h2>{description.college.name}</h2>
                     <i>{description.college.year}</i>
-                    
-                    { description.college.desc.map(i => {
-                        return <p>{i}</p>;
+                            
+                    { description.college.desc.map((i, ind) => {
+                        return <p key={ind}>{i}</p>;
                     }) }
                 </li>
 
                 <li className="flowBox">
                     <h2>{description.school.name}</h2>
                     <i>{description.school.year}</i>
-                    { description.school.desc.map(i => {
-                        return <p>{i}</p>;
+                    { description.school.desc.map((i, ind) => {
+                        return <p key={ind}>{i}</p>;
                     }) }
                 </li>
 
@@ -31,16 +31,16 @@ export default function Work() {
                 <li className="flowBox">
                     <h2>{description.hc.name}</h2>
                     <i>{description.hc.year}</i>
-                    { description.hc.desc.map(i => {
-                        return <p>{i}</p>;
+                    { description.hc.desc.map((i, ind) => {
+                        return <p key={ind}>{i}</p>;
                     }) }
                 </li>
 
                 <li className="flowBox">
                     <h2>{description.gwoc.name}</h2>
                     <i>{description.gwoc.year}</i>
-                    { description.gwoc.desc.map(i => {
-                        return <p>{i}</p>;
+                    { description.gwoc.desc.map((i, ind) => {
+                        return <p key={ind}>{i}</p>;
                     }) }
                 </li>
             </ul>
