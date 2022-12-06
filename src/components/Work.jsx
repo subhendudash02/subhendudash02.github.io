3cb656c23aa2ad968f42bdcceada383ce1990ae8
Subhendu Dash <subhendu.dash2020@vitstudent.ac.in>
2022-12-06 13:16:32 +0530

    add photos

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -5,6 +5,11 @@ import VolunteerActivismIcon from '@mui/icons-material/VolunteerActivism';
 
 import description from "../utils/description";
 
+import bg from "../assets/homebg.jpg";
+import hangman from "../assets/hangman.png";
+import num from "../assets/numerical-methods.png";
+import profile from "../assets/profile-fetcher.png";
+
 export default function Work() {
     return (
         <div className="work">
@@ -49,10 +54,12 @@ export default function Work() {
 
             <div className="projects">
                 <div className="card">
+                    <img src={hangman} alt="hangman" />
                     <h1>Hangman</h1>
                     <p>Hangman game in animal theme</p>
                 </div>
                 <div className="card">
+                    <img src={bg} alt="pyagenda" />
                     <h1>pyagenda</h1>
                     <p>pyagenda keeps track of all your activities and notifies you when they are due.</p>
                 </div>
@@ -60,10 +67,12 @@ export default function Work() {
 
             <div className="projects">
                 <div className="card">
+                    <img src={num} alt="numerical-methods" />
                     <h1>numerical methods</h1>
                     <p>This website demonstrates different iterative numerical methods to solve a single equation as well as system of equations upto 100 iterations instantly.</p>
                 </div>
                 <div className="card">
+                    <img src={profile} alt="profile-fetcher" />
                     <h1>GitHub Profile Fetcher</h1>
                     <p>	Fetches GitHub profile of a user</p>
                 </div>
