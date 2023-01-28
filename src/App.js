660dbc0988445b5aa783718b6fdaf2190966b0b5
Subhendu Dash <sdash29102@gmail.com>
2023-01-29 00:56:40 +0530

    attempt to use browserrouter

--- src/App.js
+++ src/App.js
@@ -3,20 +3,20 @@ import About from "./components/About";
 import Home from "./components/Home";
 import Work from "./components/Work";
 import Contact from "./components/Contact";
-import {Route, HashRouter, Routes} from "react-router-dom";
+import {Route, HashRouter, Routes, BrowserRouter} from "react-router-dom";
 
 function App() {
   return (
     <div className="App"> 
-      <HashRouter>
+      <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route exact path="/" element={<Home />}></Route>
+          <Route exact path="/" element={<Home />} />
           <Route exact path="/about" element={<About />} />
           <Route exact path="/work" element={<Work />} />
           <Route exact path="/contact" element={<Contact />} />
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
     </div>
   );
 }
