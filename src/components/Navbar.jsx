67f091ae73e84ee660ed1e9132df565eb942e2c1
Subhendu Dash <sdash29102@gmail.com>
2023-02-28 21:13:24 +0530

    revamp navbar fully, also responsive✨

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,16 +4,14 @@ import {Link} from "react-router-dom";
 
 export default function Navbar() {
     return (
-        <div>
-            <nav className="navbar">
-                <a href="/" className="nav-logo">Subhendu Dash</a>
-                <ul className="nav-menu">
-                    <li className="nav-item"><Link className="nav-link" to="/">Home</Link></li>
-                    <li className="nav-item"><Link className="nav-link" to="/about">About</Link></li>
-                    <li className="nav-item"><Link className="nav-link" to="/work">Work</Link></li>
-                    <li className="nav-item"><Link className="nav-link" to="/contact">Contact</Link></li>
-                </ul>
-            </nav>
-        </div>
+        <nav className="navbar">
+            <a href="/" className="nav-logo">Subhendu Dash</a>
+            <ul className="nav-menu">
+                <li className="nav-item"><Link className="nav-link" to="/">Home</Link></li>
+                <li className="nav-item"><Link className="nav-link" to="/about">About</Link></li>
+                <li className="nav-item"><Link className="nav-link" to="/work">Work</Link></li>
+                <li className="nav-item"><Link className="nav-link" to="/contact">Contact</Link></li>
+            </ul>
+        </nav>
     );
 }
