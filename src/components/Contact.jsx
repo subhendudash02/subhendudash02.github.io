c0ead8decb79f17cf7d846179628bf8d175c830a
Subhendu Dash <sdash29102@gmail.com>
2023-03-05 19:33:25 +0530

    alert msg for msg sending

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -12,8 +12,8 @@ export default function Contact() {
     const send = (e) => {
         e.preventDefault();
         emailjs.sendForm(process.env.REACT_APP_SERVICE_ID, process.env.REACT_APP_TEMPLATE_ID, sendMsg.current, process.env.REACT_APP_PUBLIC_KEY)
-            .then(() => {console.log("Sent")})
-            .catch((err) => {console.log(err)});
+            .then(() => {alert("Message sent successfully!")})
+            .catch((err) => {alert("Message not sent! Try again")});
     }
 
     return (
