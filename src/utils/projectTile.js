6bfb72e618c63aa525ffb1ae4f5609442d17d9a9
Subhendu Dash <sdash29102@gmail.com>
2023-03-18 16:52:37 +0530

    add project description

---
+++ src/utils/projectTile.js
@@ -0,0 +1,31 @@
+import workStyle from "@style/About.module.css";
+
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkIcon from '@mui/icons-material/Link';
+
+import { Stack, Typography } from "@mui/material";
+
+export default function ProjectTile(props) {
+    return (
+        <div className={workStyle.card}>
+            <img src={props.img} alt="projectTile" />
+            <h1>{props.title}</h1>
+
+            <button className={workStyle.githubLink}>
+                <Stack direction="row" alignItems="center" gap={1}>
+                    <GitHubIcon fontSize="small" color="white" />
+                    <Typography variant="p"><a href={props.githubLink} target="_blank" rel="noreferrer">GitHub</a></Typography>
+                </Stack>
+            </button>
+
+            <button className={workStyle.githubLink}>
+                <Stack direction="row" alignItems="center" gap={1}>
+                    <LinkIcon fontSize="small" color="white" />
+                    <Typography variant="p"><a href={props.visitLink} target="_blank" rel="noreferrer">Visit here</a></Typography>
+                </Stack>
+            </button>
+
+            <p>{props.description}</p>
+        </div>
+    );
+}
