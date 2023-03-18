c3d63de3396169ce0c19d2300569bded78f29bc3
Subhendu Dash <sdash29102@gmail.com>
2023-03-18 16:08:55 +0530

    add home and about

---
+++ src/utils/setParticle.js
@@ -0,0 +1,70 @@
+const config_particle = {
+    background: {
+        color: {
+            value: "#121313",
+        },
+    },
+    fpsLimit: 120,
+    interactivity: {
+        events: {
+            onHover: {
+                enable: true,
+                mode: "repulse",
+            },
+            resize: true,
+        },
+        modes: {
+            push: {
+                quantity: 4,
+            },
+            repulse: {
+                distance: 200,
+                duration: 0.4,
+            },
+        },
+    },
+    particles: {
+        color: {
+            value: "#ffffff",
+        },
+        links: {
+            color: "#ffffff",
+            distance: 150,
+            enable: true,
+            opacity: 0.5,
+            width: 1,
+        },
+        collisions: {
+            enable: true,
+        },
+        move: {
+            direction: "none",
+            enable: true,
+            outModes: {
+                default: "bounce",
+            },
+            random: false,
+            speed: 6,
+            straight: false,
+        },
+        number: {
+            density: {
+                enable: true,
+                area: 800,
+            },
+            value: 80,
+        },
+        opacity: {
+            value: 0.5,
+        },
+        shape: {
+            type: "circle",
+        },
+        size: {
+            value: { min: 1, max: 5 },
+        },
+    },
+    detectRetina: true,
+}
+
+export default config_particle;
---
+++ src/utils/themes.js
@@ -0,0 +1,11 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: "rgb(25, 207, 25)", 
+        },
+    },
+});
+
+export default theme;
