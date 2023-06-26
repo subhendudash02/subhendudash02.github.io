a308724a3d0ade738a38d46bac941c34ec8b2ffb
Subhendu Dash <sdash29102@gmail.com>
2023-06-27 01:59:12 +0530

    change home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,6 +12,9 @@ import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import { SvgIcon } from '@mui/material';
 import Head from "next/head";
+import Image from "next/image";
+
+import myphoto from '../assets/photo_me.jpeg';
 
 import Particles from "react-tsparticles";
 import { loadFull } from "tsparticles";
@@ -35,23 +38,28 @@ export default function Home() {
             </Head>
             <Navbar />
             <div className={homeStyle.home}>
-                <h1 className={homeStyle.mainHeading}>Welcome to my Portfolio!</h1>
                 <Particles className={homeStyle.particles} 
                         id="tsparticles" 
                         init={particlesInit} 
                         loaded={particlesLoaded}
                         options={config_particle} />
-                
-                <ThemeProvider theme={theme}>
-                    <a href={social.github}><GitHubIcon className={homeStyle.socIcon} fontSize="large" color="primary" /></a>
-                    <a href={social.devto}><LogoDevIcon className={homeStyle.socIcon} fontSize="large" color="primary" /></a>
-                    <a exact="true" href={social.email}><EmailRoundedIcon className={homeStyle.socIcon} fontSize="large" color="primary" /></a>
-                    <a href={social.linkedin}><LinkedInIcon className={homeStyle.socIcon} fontSize="large" color="primary" /></a>
-                    <a href={social.twitter}><TwitterIcon className={homeStyle.socIcon} fontSize="large" color="primary" /></a>
-                    <a href={social.stackoverflow}><SvgIcon className={homeStyle.socIcon} fontSize="large" color="primary">
-                        <path d="M15 21h-10v-2h10v2zm6-11.665l-1.621-9.335-1.993.346 1.62 9.335 1.994-.346zm-5.964 6.937l-9.746-.975-.186 2.016 9.755.879.177-1.92zm.538-2.587l-9.276-2.608-.526 1.954 9.306 2.5.496-1.846zm1.204-2.413l-8.297-4.864-1.029 1.743 8.298 4.865 1.028-1.744zm1.866-1.467l-5.339-7.829-1.672 1.14 5.339 7.829 1.672-1.14zm-2.644 4.195v8h-12v-8h-2v10h16v-10h-2z" />
-                        </SvgIcon></a>
-                </ThemeProvider>
+
+                <div className={homeStyle.left}>
+                    <Image alt="Avatar" src={myphoto} id={homeStyle.avatar} />
+                </div>
+                <div className={homeStyle.right}>
+                    <h2 className={homeStyle.mainHeading}>Welcome to Subu's Corner!</h2>
+                    <ThemeProvider theme={theme}>
+                        <a href={social.github}><GitHubIcon className={homeStyle.socIcon} fontSize="large" color="primary" /></a>
+                        <a href={social.devto}><LogoDevIcon className={homeStyle.socIcon} fontSize="large" color="primary" /></a>
+                        <a exact="true" href={social.email}><EmailRoundedIcon className={homeStyle.socIcon} fontSize="large" color="primary" /></a>
+                        <a href={social.linkedin}><LinkedInIcon className={homeStyle.socIcon} fontSize="large" color="primary" /></a>
+                        <a href={social.twitter}><TwitterIcon className={homeStyle.socIcon} fontSize="large" color="primary" /></a>
+                        <a href={social.stackoverflow}><SvgIcon className={homeStyle.socIcon} fontSize="large" color="primary">
+                            <path d="M15 21h-10v-2h10v2zm6-11.665l-1.621-9.335-1.993.346 1.62 9.335 1.994-.346zm-5.964 6.937l-9.746-.975-.186 2.016 9.755.879.177-1.92zm.538-2.587l-9.276-2.608-.526 1.954 9.306 2.5.496-1.846zm1.204-2.413l-8.297-4.864-1.029 1.743 8.298 4.865 1.028-1.744zm1.866-1.467l-5.339-7.829-1.672 1.14 5.339 7.829 1.672-1.14zm-2.644 4.195v8h-12v-8h-2v10h16v-10h-2z" />
+                            </SvgIcon></a>
+                    </ThemeProvider>
+                </div>
             </div>
         </>
     )
